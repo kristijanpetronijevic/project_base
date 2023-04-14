8e087b2f11cd626902e3200e47f2e58c463f33a6
kristijanpetronijevic <petronijevick29@gmail.com>
2023-04-14 20:09:24 +0200

    dodata rotacija, kod ulepsan

--- src/main.cpp
+++ src/main.cpp
@@ -310,7 +310,7 @@ int main() {
         ourShader.setVec3("viewPosition", programState->camera.Position);
         ourShader.setFloat("material.shininess", 32.0f);
         ourShader.setVec3("dirLight.direction", -0.2f, -1.0f, -0.3f);
-        ourShader.setVec3("dirLight.ambient", 0.4f, 0.4f, 0.4f);
+        ourShader.setVec3("dirLight.ambient", 0.3f, 0.3f, 0.3f);
         ourShader.setVec3("dirLight.diffuse", 0.4f, 0.4f, 0.4f);
         ourShader.setVec3("dirLight.specular", 0.5f, 0.5f, 0.5f);
         // view/projection transformations
@@ -342,7 +342,7 @@ int main() {
 //        modelPsa = glm::translate(modelPsa,
 //                                  programState->backpackPosition);//        modelPsa = glm::translate(modelPsa,
 //                                  programState->backpackPosition);
-        modelPsa = glm::translate(modelPsa, glm::vec3(22.0f, -8.0f, -33.0f));
+        modelPsa = glm::translate(modelPsa, glm::vec3(22.0f, -8.0f, -44.0f));
         modelPsa = glm::rotate(modelPsa,-1.6f, glm::vec3(1.0f, 0.0f, 0.0f));
       //  modelPsa = glm::scale(modelPsa, glm::vec3(programState->backpackScale, programState->backpackScale, programState->backpackScale));
         modelPsa = glm::scale(modelPsa, glm::vec3(0.15f, 0.15f, 0.15f));
@@ -354,8 +354,9 @@ int main() {
         ourModel1.Draw(ourShader);
 // Model sneska ispod
         glm::mat4 modelMeseca = glm::mat4(1.0f);
-        modelMeseca = glm::translate(modelMeseca ,glm::vec3(-2.0f, 7.0f, -25.0f));
-
+        modelMeseca = glm::translate(modelMeseca ,glm::vec3(10.0f, 10.0f, -46.0f));
+        modelMeseca = glm::rotate(modelMeseca,currentFrame, glm::vec3(0.0f, 1.0f, 0.0f));
+        modelMeseca = glm::translate(modelMeseca ,glm::vec3(0.0f, 0.0f, -15.0f));
         modelMeseca = glm::translate(modelMeseca ,programState->backpackPosition);
         modelMeseca= glm::scale(modelMeseca , glm::vec3(programState->backpackScale, programState->backpackScale, programState->backpackScale));
         ourShader.setMat4("model", modelMeseca);
